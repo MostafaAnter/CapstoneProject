2313582abb30ff00c861c61cf317265efc08e221
mostafa_anter <ga30000@gmail.com>
2016-12-30 01:17:21 +0200

    material splash screen

--- app/src/main/java/com/mostafa_anter/capstoneproject/ui/activity/MainActivity.java
+++ app/src/main/java/com/mostafa_anter/capstoneproject/ui/activity/MainActivity.java
@@ -54,45 +54,33 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.bbc_news) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.bbc_sport) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.cnn) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.google) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.national_geographic) {
+
+        } else if (id == R.id.sky_news) {
+
+        } else if (id == R.id.reddit) {
+
+        } else if (id == R.id.cnbc) {
+
+        } else if (id == R.id.entertainment) {
+
+        } else if (id == R.id.new_scientist) {
+
+        } else if (id == R.id.techcrunch) {
 
         }
 
